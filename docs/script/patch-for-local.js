74303015c6bb5281fa909c10bdeb14f8ddc2fbe8
Rajiv Shah <rajivshah1@icloud.com>
2018-04-08 00:20:25 -0400

    Update docs

--- docs/script/patch-for-local.js
+++ docs/script/patch-for-local.js
@@ -1,8 +1,8 @@
-(function() {
-    if (location.protocol === 'file:') {
-        var elms = document.querySelectorAll('a[href="./"]');
-        for (var i = 0; i < elms.length; i++) {
-            elms[i].href = './index.html';
-        }
+(function(){
+  if (location.protocol === 'file:') {
+    var elms = document.querySelectorAll('a[href="./"]');
+    for (var i = 0; i < elms.length; i++) {
+      elms[i].href = './index.html';
     }
+  }
 })();
